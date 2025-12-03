2a5876cca3c96a1e76bd64c591cb106ce313f4e5
Thomas O' Callaghan <tomthebomb300@gmail.com>
2025-12-03 21:37:42 +0000

    Added player to screen + flying capailities + collision detection with the floor and ceiling

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ bg_color = (128,128,128)
 lines = [0, WIDTH/4, 2*WIDTH/4, 3*WIDTH/4]
 game_speed = 2
 pause = False
+init_y = HEIGHT - 130
+player_y = init_y
+booster = False
+counter = 0
+y_velocity = 0
+gravity = 0.4
 
 def draw_screen(line_list):
     screen.fill('black')
@@ -32,17 +38,73 @@ def draw_screen(line_list):
             line_list[i] = WIDTH
     return line_list, top, bot
 
+def draw_player():
+    play = pygame.rect.Rect((120, player_y + 10), (25, 60))
+    #pygame.draw.rect(screen, 'green', play, 5)
+    if player_y < init_y or pause:
+        if booster:
+            pygame.draw.ellipse(screen, 'red', [100, player_y + 50, 20, 30])
+            pygame.draw.ellipse(screen, 'orange', [105, player_y + 50, 10, 30])
+            pygame.draw.ellipse(screen, 'yellow', [110, player_y + 50, 5, 30])
+        pygame.draw.rect(screen, 'yellow', [128, player_y + 60, 10, 20], 0, 3)
+        pygame.draw.rect(screen, 'orange', [130, player_y + 60, 10, 20], 0, 3)
+    else:
+        if counter < 10:
+            pygame.draw.line(screen, 'yellow', (128, player_y + 60), (140, player_y + 80), 10)
+            pygame.draw.line(screen, 'orange', (130, player_y + 60), (120, player_y + 80), 10)
+        elif 10 <= counter < 20:
+            pygame.draw.rect(screen, 'yellow', [128, player_y + 60, 10, 20], 0, 3)
+            pygame.draw.rect(screen, 'orange', [130, player_y + 60, 10, 20], 0, 3)
+        elif 20 <= counter < 30:
+            pygame.draw.line(screen, 'yellow', (128, player_y + 60), (120, player_y + 80), 10)
+            pygame.draw.line(screen, 'orange', (130, player_y + 60), (140, player_y + 80), 10)
+        else:
+            pygame.draw.rect(screen, 'yellow', [128, player_y + 60, 10, 20], 0, 3)
+            pygame.draw.rect(screen, 'orange', [130, player_y + 60, 10, 20], 0, 3)
 
+    pygame.draw.rect(screen, 'white', [100, player_y + 20, 20, 30], 0, 5)
+    pygame.draw.ellipse(screen, 'orange', [120, player_y + 20, 30, 50])
+    pygame.draw.circle(screen, 'orange', (135, player_y+15), 10)
+    pygame.draw.circle(screen, 'black', (138, player_y + 12), 3)
+    return play
+
+def check_colliding():
+    coll = [False, False]
+    if player.colliderect(bot_plat):
+        coll[0] = True
+    elif player.colliderect(top_plat):
+        coll[1] = True
+    return coll
 
 run = True
 
 while run:
     timer.tick(fps)
+    if counter < 40:
+        counter += 1
+    else:
+        counter = 0
     lines, top_plat, bot_plat = draw_screen(lines)
+    player = draw_player()
+    colliding = check_colliding()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not pause:
+                booster = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                booster = False
     
+    if not pause:
+        if booster:
+            y_velocity -= gravity
+        else:
+            y_velocity += gravity
+        if (colliding[0] and y_velocity > 0) or (colliding[1] and y_velocity < 0):
+            y_velocity = 0
+        player_y += y_velocity
     pygame.display.flip()
 pygame.quit()
